351d02f571a85babf8eb7022fcea9714ddea13e1
Veronchi <zhigunova1995@gmail.com>
2023-03-15 18:19:36 +0300

    feat: implement Header component

---
+++ Components/src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../assets/icons/Logo.svg';
+import './style.scss';
+
+export class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <Link to="/" className="header__logo">
+          <img src={logo} alt="logo" />
+        </Link>
+        <nav className="header__nav">
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending ? 'header__link pending' : isActive ? 'header__link active' : 'header__link'
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive, isPending }) =>
+              isPending ? 'header__link pending' : isActive ? 'header__link active' : 'header__link'
+            }
+            to="/about-us"
+          >
+            About
+          </NavLink>
+        </nav>
+      </header>
+    );
+  }
+}
