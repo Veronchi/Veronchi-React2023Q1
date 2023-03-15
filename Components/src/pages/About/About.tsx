a24eaff9c921e7b189cf1e6df299eb4f9915acc8
Veronchi <zhigunova1995@gmail.com>
2023-03-15 18:32:19 +0300

    feat: add pages start layout

---
+++ Components/src/pages/About/About.tsx
@@ -0,0 +1,8 @@
+import React, { Component, ReactNode } from 'react';
+import './style.scss';
+
+export class About extends Component {
+  render(): ReactNode {
+    return <main className="about">about</main>;
+  }
+}
---
+++ Components/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import { Component, ReactNode } from 'react';
+import './style.scss';
+
+export class MainPage extends Component {
+  render(): ReactNode {
+    return <main className="main">main</main>;
+  }
+}
