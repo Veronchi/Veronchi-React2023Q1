eb1b4d6d0edd161e528a0f6704d8348a1d65af61
Veronchi <zhigunova1995@gmail.com>
2023-03-15 18:17:13 +0300

    feat: add router

--- Components/src/App.tsx
+++ Components/src/App.tsx
@@ -1,7 +1,28 @@
-import './App.css';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { MainPage } from './pages/MainPage/MainPage';
+import { About } from './pages/About/About';
+import { NotFound } from './pages/NotFound/NotFound';
+import './App.scss';
 
-function App() {
-  return <div className="App">Components</div>;
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="about-us" element={<About />} />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+const App = () => <RouterProvider router={router} />;
 
 export default App;
