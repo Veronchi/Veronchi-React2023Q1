e8ea24ffddb9774669eb34af726adb2a5084b827
Veronchi <zhigunova1995@gmail.com>
2023-03-18 00:35:50 +0300

    feat: add products list

--- Components/src/utils/interfaces.ts
+++ Components/src/utils/interfaces.ts
@@ -3,3 +3,14 @@ export type SearchProps = Record<string, never>;
 export interface SearchValue {
   value: string;
 }
+
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  oldPrice: number | undefined;
+  newPrice: number;
+  rating: string;
+  isLiked: boolean;
+  img: string;
+}
