f2d4c81c50aea0ba06585c12de27182eea06f395
Veronchi <zhigunova1995@gmail.com>
2023-03-17 13:26:21 +0300

    refactor: change to classes

--- Components/src/components/Footer/Footer.tsx
+++ Components/src/components/Footer/Footer.tsx
@@ -1,30 +1,32 @@
-import React from 'react';
+import { Component } from 'react';
 import rssIcon from '../../assets/icons/rs_school_js.svg';
 import './style.scss';
 
-export const Footer = (): JSX.Element => {
-  return (
-    <footer className="footer">
-      <ul className="footer__list">
-        <li className="footer__item">
-          <span>&copy; 2023</span>
-        </li>
-        <li className="footer__item">
-          <a
-            className="footer__link"
-            href="https://github.com/Veronchi"
-            target="_blank"
-            rel="author noreferrer"
-          >
-            Veronchi
-          </a>
-        </li>
-        <li className="footer__item">
-          <a href="https://rs.school/react/" target="_blank" rel="noreferrer">
-            <img className="footer__img" src={rssIcon} alt="rsschool" />
-          </a>
-        </li>
-      </ul>
-    </footer>
-  );
-};
+export class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <ul className="footer__list">
+          <li className="footer__item">
+            <span>&copy; 2023</span>
+          </li>
+          <li className="footer__item">
+            <a
+              className="footer__link"
+              href="https://github.com/Veronchi"
+              target="_blank"
+              rel="author noreferrer"
+            >
+              Veronchi
+            </a>
+          </li>
+          <li className="footer__item">
+            <a href="https://rs.school/react/" target="_blank" rel="noreferrer">
+              <img className="footer__img" src={rssIcon} alt="rsschool" />
+            </a>
+          </li>
+        </ul>
+      </footer>
+    );
+  }
+}
--- Components/src/pages/NotFound/NotFound.tsx
+++ Components/src/pages/NotFound/NotFound.tsx
@@ -1,17 +1,20 @@
+import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './style.scss';
 
-export const NotFound = () => {
-  return (
-    <section className="notfound">
-      <h1 className="notfound__title">404</h1>
-      <h3 className="notfound__subtitle">Page not found</h3>
-      <p className="notfound__text">
-        Go to the{' '}
-        <Link className="notfound__link" to="/">
-          Homepage
-        </Link>
-      </p>
-    </section>
-  );
-};
+export class NotFound extends Component {
+  render() {
+    return (
+      <section className="notfound">
+        <h1 className="notfound__title">404</h1>
+        <h3 className="notfound__subtitle">Page not found</h3>
+        <p className="notfound__text">
+          Go to the{' '}
+          <Link className="notfound__link" to="/">
+            Homepage
+          </Link>
+        </p>
+      </section>
+    );
+  }
+}
