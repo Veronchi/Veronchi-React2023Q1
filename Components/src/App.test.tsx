c0a686bdb24835826993c6b2c196469089337193
Veronchi <zhigunova1995@gmail.com>
2023-03-19 18:49:44 +0300

    refactor: clean test code

--- Components/src/App.test.tsx
+++ Components/src/App.test.tsx
@@ -5,8 +5,11 @@ import App from './App';
 import { BrowserRouter, MemoryRouter } from 'react-router-dom';
 
 describe('Router', () => {
-  it('Click the about router link', async () => {
+  beforeEach(() => {
     render(<App />, { wrapper: BrowserRouter });
+  });
+
+  it('Click the about router link', async () => {
     expect(screen.getByText('About')).toBeInTheDocument();
     const user = userEvent.setup();
     const about = vi.spyOn(user, 'click');
@@ -14,8 +17,8 @@ describe('Router', () => {
     await user.click(aboutLink);
     expect(about).toHaveBeenCalledTimes(1);
   });
+
   it('Click the home router link', async () => {
-    render(<App />, { wrapper: BrowserRouter });
     expect(screen.getByText('Home')).toBeInTheDocument();
     const user = userEvent.setup();
     const home = vi.spyOn(user, 'click');
@@ -23,6 +26,7 @@ describe('Router', () => {
     await user.click(homeLink);
     expect(home).toHaveBeenCalledTimes(1);
   });
+
   it('notFound page', () => {
     render(
       <MemoryRouter initialEntries={['/*']}>
