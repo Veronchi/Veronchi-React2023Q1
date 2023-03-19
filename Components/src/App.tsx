e5989275facaee35225f0bd33599b998cae5d79c
Veronchi <zhigunova1995@gmail.com>
2023-03-19 18:07:45 +0300

    refactor: change router

--- Components/src/App.tsx
+++ Components/src/App.tsx
@@ -1,28 +1,19 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { MainPage } from './pages/MainPage/MainPage';
 import { About } from './pages/About/About';
 import { NotFound } from './pages/NotFound/NotFound';
 import './App.scss';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<MainPage />} />
-        <Route path="about-us" element={<About />} />
-      </Route>
-
-      <Route path="*" element={<NotFound />} />
+const App = (): JSX.Element => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<MainPage />} />
+      <Route path="about-us" element={<About />} />
     </Route>
-  )
-);
 
-const App = () => <RouterProvider router={router} />;
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+);
 
 export default App;
