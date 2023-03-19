3f471564856654b6b733764b2750f206bdd618da
Veronchi <zhigunova1995@gmail.com>
2023-03-19 18:11:58 +0300

    feat: add testing configs

--- Components/vite.config.ts
+++ Components/vite.config.ts
@@ -1,7 +1,23 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
+
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      exclude: [...configDefaults.coverage.exclude, 'src/main.tsx'],
+      all: true,
+      src: ['src'],
+      provider: 'c8',
+      reporter: ['text'],
+    },
+  },
 });
