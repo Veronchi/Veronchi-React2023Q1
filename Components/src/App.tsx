31f7fde48d6ecc3a072ebc375b07aa45c83d1df3
Veronchi <zhigunova1995@gmail.com>
2023-03-21 00:48:41 +0300

    feat: add new route

--- Components/src/App.tsx
+++ Components/src/App.tsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { MainPage } from './pages/MainPage/MainPage';
-import { About } from './pages/About/About';
-import { NotFound } from './pages/NotFound/NotFound';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Layout, Form } from '@/components';
+import { About, MainPage, NotFound } from '@/pages';
 import './App.scss';
 
 const App = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<Layout />}>
-      <Route index element={<MainPage />} />
+      <Route path="home" element={<MainPage />} />
+      <Route index element={<Navigate to="home" replace />} />
       <Route path="about-us" element={<About />} />
+      <Route path="from" element={<Form />} />
     </Route>
 
     <Route path="*" element={<NotFound />} />
