912f57626513fabf880bef7bf5786d87ca0b8971
Veronchi <zhigunova1995@gmail.com>
2023-03-23 14:45:20 +0300

    feat: add interfaces

--- Components/src/utils/interfaces.ts
+++ Components/src/utils/interfaces.ts
@@ -1,3 +1,5 @@
+import { forwardRef, RefObject } from 'react';
+
 export type SearchProps = Record<string, never>;
 
 export interface SearchValue {
@@ -17,4 +19,61 @@ export interface Product {
   rating: string;
   isLiked: boolean;
   img: string;
+}
+
+export interface FormItem {
+  id?: number;
+  title: string;
+  description: string;
+  price: string;
+  collection: string;
+  forWhom: string;
+  isFavorite: boolean;
+  img: string;
+}
+
+export type FormPageProps = Record<string, never>;
+
+export interface FormPageState {
+  items: FormItem[];
+}
+
+export interface FormProps {
+  addItem: (item: FormItem) => void;
+}
+
+export interface FormState {
+  isValid: boolean;
+  isSucceed: boolean;
+}
+
+export interface FormRefs {
+  form: RefObject<HTMLFormElement>;
+  title: RefObject<HTMLInputElement>;
+  description: RefObject<HTMLTextAreaElement>;
+  category: RefObject<HTMLSelectElement>;
+  price: RefObject<HTMLInputElement>;
+  collection: RefObject<HTMLInputElement>;
+  forMen: RefObject<HTMLInputElement>;
+  forWomen: RefObject<HTMLInputElement>;
+  isFavorite: RefObject<HTMLInputElement>;
+  img: RefObject<HTMLInputElement>;
+}
+
+export type InputState = Record<string, never>;
+
+export interface InputProps {
+  forwardRef: RefObject<HTMLInputElement>;
+}
+
+export interface TextAreaProps {
+  forwardRef: RefObject<HTMLTextAreaElement>;
+}
+
+export interface SelectProps {
+  forwardRef: RefObject<HTMLSelectElement>;
+}
+
+export interface RadioProps {
+  forwardRef: { forMen: RefObject<HTMLInputElement>; forWomen: RefObject<HTMLInputElement> };
 }
