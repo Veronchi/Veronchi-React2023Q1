e46d0ac7cc13e83ecfcb9e8a0c40906f3e07126a
Veronchi <zhigunova1995@gmail.com>
2023-03-23 14:46:44 +0300

    feat: add imports

--- Components/src/components/index.ts
+++ Components/src/components/index.ts
@@ -3,5 +3,33 @@ import { Footer } from './Footer/Footer';
 import { SearchBar } from './SearchBar/SearchBar';
 import { CardList } from './CardList/CardList';
 import { Card } from './Card/Card';
+import { Layout } from './Layout';
+import { Form } from './Form/Form';
+import { CategoryInput } from './inputs/CategoryInput/CategoryInput';
+import { CheckInput } from './inputs/CheckInput/CheckInput';
+import { SelectInput } from './inputs/SelectInput/SelectInput';
+import { DescInput } from './inputs/DescInput/DescInput';
+import { FileInput } from './inputs/FileInput/FileInput';
+import { PriceInput } from './inputs/PriceInput/PriceInput';
+import { RadioInput } from './inputs/RadioInput/RadioInput';
+import { TitleInput } from './inputs/TitleInput/TitleInput';
+import { DateInput } from './inputs/DateInput/DateInput';
 
-export { Header, Footer, SearchBar, CardList, Card };
+export {
+  TitleInput,
+  Layout,
+  Header,
+  Footer,
+  SearchBar,
+  CardList,
+  Card,
+  Form,
+  CategoryInput,
+  CheckInput,
+  SelectInput,
+  DescInput,
+  FileInput,
+  PriceInput,
+  RadioInput,
+  DateInput,
+};
