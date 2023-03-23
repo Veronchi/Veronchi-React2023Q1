94e577ba611a9cf3611afae3d069836e8fe337ea
Veronchi <zhigunova1995@gmail.com>
2023-03-23 14:44:51 +0300

    feat: add check input component

---
+++ Components/src/components/inputs/CheckInput/CheckInput.tsx
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import { InputProps, InputState } from '@/utils/interfaces';
+
+export class CheckInput extends Component<InputProps, InputState> {
+  render() {
+    const { forwardRef } = this.props;
+
+    return (
+      <div className="form__input">
+        <label htmlFor="agree">Add to favorite?</label>
+        <input type="checkbox" ref={forwardRef} defaultChecked={false} required name="agree" />
+      </div>
+    );
+  }
+}
