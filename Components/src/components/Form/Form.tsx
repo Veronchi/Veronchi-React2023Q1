87dd9a7657112826c843d65b4216d3b16d5fbca0
Veronchi <zhigunova1995@gmail.com>
2023-03-24 23:08:36 +0300

    feat: implement form processing

--- Components/src/components/Form/Form.tsx
+++ Components/src/components/Form/Form.tsx
@@ -10,7 +10,15 @@ import {
   SelectInput,
   TitleInput,
 } from '../index';
-import { validateSelect } from '@/validation/validation';
+import {
+  validateDate,
+  validateDesc,
+  validateFile,
+  validatePrice,
+  validateRadio,
+  validateSelect,
+  validateTitle,
+} from '@/validation/validation';
 import './style.scss';
 
 export class Form extends Component<FormProps, FormState> {
@@ -35,37 +43,68 @@ export class Form extends Component<FormProps, FormState> {
     };
   }
 
-  checkValid = (): void => {
-    const { category } = this.refObj;
+  checkValid = async (): Promise<boolean> => {
+    const { title, category, description, price, collection, forMen, forWomen, img } = this.refObj;
+    const titleError = validateTitle(title.current?.value as string);
+    const descError = validateDesc(description.current?.value as string);
     const selectError = validateSelect(category.current?.value as string);
+    const priceError = validatePrice(price.current?.value as string);
+    const dateError = validateDate(collection.current?.value as string);
+    const radioError = validateRadio({
+      men: forMen.current?.checked as boolean,
+      women: forWomen.current?.checked as boolean,
+    });
+    const fileError = validateFile(img.current?.files as FileList);
 
-    if (selectError) {
-      this.setState({ errors: [...this.state.errors, selectError] });
-    }
+    const arr = [
+      titleError,
+      descError,
+      selectError,
+      priceError,
+      dateError,
+      radioError,
+      fileError,
+    ].filter((item) => item) as string[];
+
+    await this.setState({ errors: arr });
+
+    const isValid = this.state.errors.length < 1;
+
+    if (isValid) await this.setState({ errors: [] });
+
+    return isValid;
   };
 
   handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+    const { title, category, description, price, collection, forMen, forWomen, isFavorite, img } =
+      this.refObj;
+    const titleStr = title.current?.value as string;
+    const categoryStr = category.current?.value as string;
+    const descriptionStr = description.current?.value as string;
+    const priceStr = price.current?.value as string;
+    const collectionStr = collection.current?.value as string;
+    const radio = forMen || forWomen;
+    const forWhom = radio.current?.checked as boolean;
+    const checkbox = isFavorite.current?.checked as boolean;
 
-    this.checkValid();
-    // const file = this.refObj.img.current?.files?.[0] as Blob;
-    // const fileObj = URL.createObjectURL(file);
+    this.checkValid().then((isValid: boolean) => {
+      if (isValid) {
+        const file = img.current?.files?.[0] as Blob;
+        const imgFile = URL.createObjectURL(file);
 
-    // const { category } = this.refObj;
-    // this.checkValid(category.current?.value as string);
-
-    // const isValidSelect = validateSelect(category.current?.value as string);
-    // console.log(isValidSelect);
-
-    // console.log(this.refObj.title.current?.value);
-    // console.log(this.refObj.description.current?.value);
-    // console.log(this.refObj.category.current?.value);
-    // console.log(this.refObj.collection.current?.value);
-    // console.log(this.refObj.price.current?.value);
-    // console.log(this.refObj.forMen.current?.checked);
-    // console.dir(this.refObj.forWomen.current?.checked);
-    // console.log(this.refObj.isFavorite.current?.checked);
-    // console.log(this.refObj.img.current?.files?.[0]);
+        this.props.addItem({
+          title: titleStr,
+          description: descriptionStr,
+          category: categoryStr,
+          price: priceStr,
+          collection: collectionStr,
+          forWhom,
+          img: imgFile,
+          isFavorite: checkbox,
+        });
+      }
+    });
   };
 
   render() {
@@ -83,15 +122,18 @@ export class Form extends Component<FormProps, FormState> {
     } = this.refObj;
 
     return (
-      <form className="form" ref={form} onSubmit={(e) => this.handleSubmit(e)}>
-        <TitleInput forwardRef={title} />
-        <DescInput forwardRef={description} />
+      <form className="form" ref={form} noValidate onSubmit={(e) => this.handleSubmit(e)}>
+        <TitleInput forwardRef={title} errors={this.state.errors} />
+        <DescInput forwardRef={description} errors={this.state.errors} />
         <SelectInput forwardRef={category} errors={this.state.errors} />
-        <PriceInput forwardRef={price} />
-        <RadioInput forwardRef={{ forMen: forMen, forWomen: forWomen }} />
-        <DateInput forwardRef={collection} />
-        <FileInput forwardRef={img} />
-        <CheckInput forwardRef={isFavorite} />
+        <PriceInput forwardRef={price} errors={this.state.errors} />
+        <RadioInput
+          forwardRef={{ forMen: forMen, forWomen: forWomen }}
+          errors={this.state.errors}
+        />
+        <DateInput forwardRef={collection} errors={this.state.errors} />
+        <FileInput forwardRef={img} errors={this.state.errors} />
+        <CheckInput forwardRef={isFavorite} errors={this.state.errors} />
 
         <input type="submit" />
       </form>
