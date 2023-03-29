af3091a2ebc10d3c2fa480de80e708a6028afd05
Veronchi <zhigunova1995@gmail.com>
2023-03-29 23:55:07 +0300

    feat: rewrite card list component with hooks

--- Components/src/components/CardList/CardList.tsx
+++ Components/src/components/CardList/CardList.tsx
@@ -1,16 +1,14 @@
-import { Component } from 'react';
+import { FC } from 'react';
 import { Card } from '../index';
 import { products } from '@/utils/mockData';
 import './style.scss';
 
-export class CardList extends Component {
-  render() {
-    return (
-      <ul className="products" data-testid="products-list">
-        {products.map((item) => (
-          <Card key={item.id} {...item} />
-        ))}
-      </ul>
-    );
-  }
-}
+export const CardList: FC = () => {
+  return (
+    <ul className="products" data-testid="products-list">
+      {products.map((item) => (
+        <Card key={item.id} {...item} />
+      ))}
+    </ul>
+  );
+};
