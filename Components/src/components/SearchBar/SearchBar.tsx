3d82f07c49c65268f18499b11965fe55cf4b8b79
Veronchi <zhigunova1995@gmail.com>
2023-04-03 19:42:52 +0300

    refactor: rewrite with useRef

--- Components/src/components/SearchBar/SearchBar.tsx
+++ Components/src/components/SearchBar/SearchBar.tsx
@@ -1,35 +1,25 @@
-import { ChangeEvent, FC, useEffect, useRef, useState } from 'react';
+import { FC, RefObject, useEffect, useRef } from 'react';
 import './style.scss';
 
 export const SearchBar: FC = () => {
-  const [searchValue, setSearchValue] = useState(localStorage.getItem('inputValue') || '');
-  const inputRef = useRef(searchValue);
+  const inputRef: RefObject<HTMLInputElement> = useRef(null);
 
   useEffect(() => {
+    const input = inputRef.current;
+
+    if (input) {
+      input.value = localStorage.getItem('inputValue') || '';
+    }
+
     return () => {
-      localStorage.setItem('inputValue', inputRef.current || '');
+      localStorage.setItem('inputValue', input?.value || '');
     };
   }, []);
-
-  useEffect(() => {
-    inputRef.current = searchValue;
-  }, [searchValue]);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const inputValue = e.target.value;
-    setSearchValue(inputValue);
-  };
 
   return (
     <form className="search">
       <span className="input-container">
-        <input
-          className="search__input"
-          type="text"
-          placeholder="Search"
-          value={searchValue}
-          onChange={(e) => handleChange(e)}
-        />
+        <input className="search__input" type="text" placeholder="Search" ref={inputRef} />
       </span>
     </form>
   );
